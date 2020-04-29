d6f5667b4946ca24ecec377cb5feec9f3d544da0
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-04-29 15:36:18 +0500

    Update setup.py

---
+++ the final task/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='file sorter',
+    version='1.0',
+    install_requires=['file sorter'],
+    emtry_points='''
+    [console_scripts]
+    hello=file sorter:hello
+    '''
+)
