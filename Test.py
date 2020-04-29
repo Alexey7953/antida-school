1073184e275cb93558800a74e73101ec58c779be
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-04-29 20:04:07 +0500

    Update Test.py

--- Test.py
+++
@@ -1,9 +0,0 @@
-class Employee(object):
-
-    def __init__(self, base_payment, prize, hour):
-        self.base_payment = float(base_payment)
-        self.hour = int(hour)
-        self.prize = float(prize)
-
-    def _payment(self):
-        return self.base_payment
