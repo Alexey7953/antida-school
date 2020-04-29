b01eec7251050b259c87685e51eedc065236754d
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-04-29 23:11:15 +0500

    Update Family Tree.py

--- other tasks/Family Tree.py
+++ other tasks/Family Tree.py
@@ -1,8 +1,8 @@
 class familytree(object):
-    def __init__(self, firstname, surname, birthday, baby):
-        self.first_name = int(firstname)
+    def __init__(self, first_name, surname, birthday, baby):
+        self.firstname = int(first_name)
         self.surname = int(surname)
-        self.birthday = input(birthday)
+        self.birthday = str(birthday)
         self.baby = input(baby)
 
     def _parents(self):
