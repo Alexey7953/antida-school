40c1d17abe3d205cb1150cd10b0f631c4c87d4ab
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-05-02 14:34:23 +0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,20 +1,4 @@
-import sys
-import argparse
-
-
-def createParser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--name', type=open)
-
-    return parser
-
-
-if __name__ == '__main__':
-    parser = createParser()
-    namespace = parser.parse_args(sys.argv[1:])
-
-    print(namespace)
-
-    text = namespace.name.read()
-
-    print(text)
+import songdetails
+song = songdetails.scan("blah.mp3")
+if song is not None:
+    print (song.artist)
