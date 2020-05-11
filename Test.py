f744b9eb0a700e4a5e5499843f0f0abaf87226a3
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-05-12 02:06:21 +0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -11,3 +11,11 @@ cursor.execute(
     "password 	TEXT    NOT NULL"
     ");"
 )
+query = "INSERT INTO account (first_name, last_name, email, password) VALUES (?, ?, ?, ?);"
+user_data = [
+    ("Евгений", "Пирожков", "ex-1@mail.ru", "123465"),
+    ("Макар", "Петрович", "ex-2@mail.ru", "123465"),
+    ("Иванов", "Семенов", "ex-3@mail.ru", "123465"),
+    ("Алена", "Петрова", "ex-4@mail.ru", "123465"),
+]
+cursor.executemany(query, user_data)
