e9a2335d2d3824e74a2d3b72c3dafbbedc36d179
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-05-21 23:00:42 +0500

    Update 9.1.py

--- Lesson 11/9.1.py
+++ Lesson 11/9.1.py
@@ -43,9 +43,103 @@
 Задача считается решенной, если после выполнения программы в базе создатутся
 новые отношения и они будут соотв. физической модели.
 """
-
 import sqlite3
+with sqlite3.connect('example.db') as connection:
+    cursor = connection.cursor()
 
-with sqlite3.connect('data.db') as connection:
-    c = connection.cursor()
-    c.execute()
+    # 1 Создание таблицы Account
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS account ("
+        "id 		INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "first_name TEXT    NOT NULL,"
+        "last_name 	TEXT    NOT NULL,"
+        "email 		TEXT    NOT NULL UNIQUE,"
+        "password 	TEXT    NOT NULL"
+        ");"
+    )
+    # 2 Создание таблицы City
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS city ("
+        "id 	INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "name   TEXT NOT NULL"
+        ");"
+    )
+    # 3 Создание таблицы ZipCode
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS zipcode ("
+        "zip_code	 INTEGER PRIMARY KEY,"
+        "city_id     INTEGER NOT NULL REFERENCES city(id)"
+        ");"
+    )
+    # 4 Создание таблицы Seller
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS seller ("
+        "id 		 INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "zip_code    INTEGER NOT NULL REFERENCES zipcode(zip_code),"
+        "street      TEXT NOT NULL,"
+        "home        TEXT NOT NULL,"
+        "phone       TEXT NOT NULL,"
+        "account_id  INTEGER NOT NULL REFERENCES account(id)"
+        ");"
+    )
+    # 5 Создание таблицы Color
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS color ("
+        "id     INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "name   TEXT NOT NULL,"
+        "hex    TEXT NOT NULL"
+        ");"
+    )
+    # 6 Создание таблицы Car
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS car ("
+        "id                 INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "make               TEXT NOT NULL,"
+        "model              TEXT NOT NULL,"
+        "mileage            INTEGER  NOT NULL,"
+        "num_owners         INTEGER NOT NULL DEFAULT 1,"
+        "reg_number         TEXT NOT NULL"
+        ");"
+    )
+    # 7 Создание таблицы CarColor
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS car_color("
+        "id         INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "color_id   INTEGER NOT NULL REFERENCES color(id),"
+        "car_id     INTEGER NOT NULL REFERENCES car(id)"
+        ");"
+    )
+    # 8 Создание таблицы Ad
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS ad ("
+        "id         INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "title      TEXT NOT NULL,"
+        "date       INTEGER NOT NULL,"
+        "seller_id  INTEGER NOT NULL REFERENCES seller(id),"
+        "car_id     INTEGER NOT NULL REFERENCES car(id)"
+        ");"
+    )
+    # 9 Создание таблицы Image
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS image ("
+        "id         INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "title      TEXT,"
+        "url        TEXT NOT NULL,"
+        "car_id     INTEGER NOT NULL REFERENCES car(id)"
+        ");"
+    )
+    # 10 Создание таблицы Tag
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS tag ("
+        "id         INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "name       TEXT NOT NULL UNIQUE"
+        ");"
+    )
+    # 11 Создание таблицы AdTag
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS ad_tag ("
+        "id         INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "tag_id     INTEGER NOT NULL REFERENCES tag(id),"
+        "ad_id      INTEGER NOT NULL REFERENCES ad(id)"
+        ");"
+    )
