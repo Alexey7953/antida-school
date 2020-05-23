caae5237af28358d9ff1115cfba7c4e62ab64434
Alexey <zakharov_alexey@mail.ru>
2020-05-24 00:19:39 +0500

    ...

--- Lesson 11/10.1.py
+++ Lesson 11/10.1.py
@@ -49,103 +49,43 @@ from datetime import datetime
 with sqlite3.connect('example.db') as connection:
     cursor = connection.cursor()
 
-    # Добавление данных в таблицу Account
-    # Пример использования executemany()
-    data = [
-        ('Евгений', 'Разгуляев', 'box-1@mail.ru', 'secret-password'),
-        ('Алекс', 'Миллер', 'box-2@mail.ru', 'secret-password'),
-        ('Иванов', 'Семенов', 'box-3@mail.ru', 'secret-password'),
-        ('Алена', 'Петрова', 'box-4@mail.ru', 'secret-password')
-    ]
-    query = """
-        INSERT INTO account (first_name, last_name, email, password)
-        VALUES (?, ?, ?, ?)
-    """
-    cursor.executemany(query, data)
-
-    # Добавление данных в таблицу City
-    # Пример использования execute()
-    cursor.execute("""
-        INSERT INTO city (name) 
-        VALUES 
-        ('Челябинск'),
-        ('Москва'),
-        ('Владивосток');
-   """)
-
-    # Добавление данных в таблицы ZipCode, Seller, Color, Car, CarColor
-    # Пример использования executescript()
-    cursor.executescript("""
-        INSERT INTO zipcode (zip_code, city_id) 
-        VALUES 
-        ('454000', 1),
-        ('101000', 2),
-        ('690000', 3);
-
-        INSERT INTO seller (zip_code, street, home, phone, account_id) 
-        VALUES 
-        ('454000', 'Раздольная', '5/A', '86541237845', 1),
-        ('101000', 'Ленина', '125', '89198971212', 3);
-
-        INSERT INTO color (name, hex) 
-        VALUES 
-        ('красный', 'ff0000'),
-        ('синий', '000066'),
-        ('зеленый', '339966'),
-        ('желтый', 'FFFF00'),
-        ('черный', '000000');
-
-        INSERT INTO car (make, model, mileage, num_owners, reg_number)
-        VALUES 
-        ('Nissan', 'Patrol', '40500', 5, 'o423oo'),
-        ('Mercedes', 'GLA', '5000', 1, 'x666xx'),
-        ('ВАЗ', '2110', '10000', 2, 'м123ск'),
-        ('УАЗ', 'Patriot', '25550', 3, 'п564оп'),
-        ('Ford', 'GT', '100000', 4, 'к013ар'),
-        ('Chevrolet', 'Impala 1965', '150000', 2, 'и555мп');
-
-        INSERT INTO carcolor (color_id, car_id) 
-        VALUES 
-        ('1', '1'), 
-        ('2', '1'), 
-        ('3', '1'), 
-        ('1', '2'), 
-        ('2', '3'), 
-        ('2', '4'),
-        ('3', '4');    
-    """)
-
-    # Добавление данных в таблицу Ad
-    # Пример использования форматированных строк
-    date_1 = int(datetime(year=2019, month=12, day=1).timestamp())
-    date_2 = int(datetime(year=2020, month=1, day=1, hour=12).timestamp())
-    date_3 = int(datetime(year=2020, month=1, day=15).timestamp())
-    date_4 = int(datetime(year=2018, month=2, day=27).timestamp())
-    date_5 = int(datetime(year=2020, month=3, day=13).timestamp())
-
-    cursor.execute(f"""
-        INSERT INTO ad (title, date, car_id, seller_id) 
-        VALUES 
-        ('Продам внедорожник', {date_1}, 1, 1),
-        ('Продам внедорожник с пробегом', {date_2}, 1, 1),
-        ('Продам мерседес после восстановления', {date_3}, 2, 2),
-        ('Десятка в хорошем состоянии', {date_4}, 3, 2),
-        ('Уаз в люкс комплектации', {date_5}, 4, 2);
-    """)
-
     # Добавление данных в таблицу Image
-    cursor.execute("""
-    INSERT INTO ad (id, title, url, car_id)
-    VALUES 
-    ()
-    """)
+    image_data = [
+        ('Картина 1', 'D:/pics', '1'),
+        ('Картина 2', 'D:/pics', '2'),
+        ('Картина 3', 'D:/pics', '3'),
+        ('Картина 4', 'D:/pics', '4'),
+        ('Картина 5', 'D:/pics', '5')
+    ]
+    image_query = """
+     INSERT INTO Image (title, url, car_id)
+     VALUES (?,?,?)
+     """
+    cursor.executemany(image_query, image_data)
+
     # Добавление данных в таблицу Tag
-    cursor.execute("""
-    INSERT INTO ad (id, name)
-    ()
-    """)
+    tag_data = [
+        ('one',),
+        ('two',),
+        ('three',),
+        ('four',),
+        ('five',)
+    ]
+    tag_query = """
+    INSERT INTO Tag (name)
+    VALUES (?)
+    """
+    cursor.executemany(tag_query, tag_data)
     # Добавление данных в таблицу AdTag
-    cursor.execute("""
-    INSERT INTO ad (id, tag_id, ad_id)
-    ()
-    """)
+    adTag_data = [
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5')
+    ]
+    adTag_query = """
+    INSERT INTO ad_tag (Tag_id, Ad_id)
+    VALUES (?,?)
+    """
+    cursor.executemany(adTag_query, adTag_data)
--- Test.py
+++ Test.py
@@ -1,21 +1,4 @@
 import sqlite3
 
-connection = sqlite3.connect('example.db')
-cursor = connection.cursor()
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS account ("
-    "id 		INTEGER PRIMARY KEY AUTOINCREMENT,"
-    "first_name TEXT    NOT NULL,"
-    "last_name 	TEXT    NOT NULL,"
-    "email 		TEXT    NOT NULL UNIQUE,"
-    "password 	TEXT    NOT NULL"
-    ");"
-)
-query = "INSERT INTO account (first_name, last_name, email, password) VALUES (?, ?, ?, ?);"
-user_data = [
-    ("Евгений", "Пирожков", "ex-1@mail.ru", "123465"),
-    ("Макар", "Петрович", "ex-2@mail.ru", "123465"),
-    ("Иванов", "Семенов", "ex-3@mail.ru", "123465"),
-    ("Алена", "Петрова", "ex-4@mail.ru", "123465"),
-]
-cursor.executemany(query, user_data)
+with sqlite3.connect('example.db') as connection:
+    cursor = connection.cursor()
