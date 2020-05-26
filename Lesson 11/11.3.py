499bf097aa1460bfb4e16458d1719551ad248d0a
Alexey <zakharov_alexey@mail.ru>
2020-05-26 21:35:19 +0500

    ...

--- Lesson 11/11.3.py
+++ Lesson 11/11.3.py
@@ -26,9 +26,7 @@ import sqlite3
 with sqlite3.connect('example.db') as connection:
 
     cursor = connection.cursor()
-    cursor.execute("""
-    
-    """)
+
     cursor.execute(
         "SELECT color.name, make, model "
         "FROM car "
