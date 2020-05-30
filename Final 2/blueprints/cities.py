354142228d19938ad6e91d3a9028a831c4b642a8
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-05-30 12:01:56 +0500

    Update cities.py

--- Final 2/blueprints/cities.py
+++ Final 2/blueprints/cities.py
@@ -19,13 +19,15 @@ bp = Blueprint('cities', __name__)
 
 class CitiesView(MethodView):
     # Получение списка всех городов
-    def get(self):
+    @staticmethod
+    def get():
         with db.connection as connection:
             service = CitiesService(connection)
             cities = service.read_all()
             return jsonify(cities), 200
 
-    def post(self):
+    @staticmethod
+    def post():
         # Создание нового города
 
         name = request.json.get("name")
