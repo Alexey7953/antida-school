56a9203ea535eabbf0c916a457acc3833dc6f0a2
Alexey <zakharov_alexey@mail.ru>
2020-06-01 03:29:26 +0500

    create image

--- Lesson 13/final v 2.0/src/app.py
+++ Lesson 13/final v 2.0/src/app.py
@@ -3,6 +3,7 @@ from src.db import close_db
 from src.database import db
 from blueprints.cities import bp as bp_cities
 from blueprints.colors import bp as bp_color
+from blueprints.image import bp as bp_image
 
 
 def create_app():
@@ -11,6 +12,6 @@ def create_app():
     app.teardown_appcontext(close_db)
     app.register_blueprint(bp_color, url_prefix='/color')
     app.register_blueprint(bp_cities, url_prefix='/cities')
-
+    app.register_blueprint(bp_image, url_prefix='/image')
     db.init_app(app)
     return app
