cfd4dcf6499dfce849e0edfd1de73478db34f956
Alexey <zakharov_alexey@mail.ru>
2020-06-01 03:46:01 +0500

    clean

--- Lesson 13/final v 2.0/src/app.py
+++ Lesson 13/final v 2.0/src/app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from src.db import close_db
 from src.database import db
 from blueprints.cities import bp as bp_cities
 from blueprints.colors import bp as bp_color
@@ -9,7 +8,6 @@ from blueprints.image import bp as bp_image
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
-    app.teardown_appcontext(close_db)
     app.register_blueprint(bp_color, url_prefix='/color')
     app.register_blueprint(bp_cities, url_prefix='/cities')
     app.register_blueprint(bp_image, url_prefix='/image')
