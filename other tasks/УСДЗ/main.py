d6eed92192cd0e769fec8e75bd103120e31c14c1
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-05-31 12:17:04 +0500

    Delete main.py

--- other tasks/УСДЗ/main.py
+++
@@ -1,9 +0,0 @@
-
-# Create application
-    import sys
-    app = QtGui.QApplication(sys.argv)
-    USDZ = QtGui.QDialog()
-    ui = Ui_USDZ()
-    ui.setupUi(USDZ)
-    USDZ.show()
-    sys.exit(app.exec_())
