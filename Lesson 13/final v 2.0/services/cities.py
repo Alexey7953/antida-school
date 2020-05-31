bfce78958d716378a2b479f272fe28fface5ba01
Alexey <zakharov_alexey@mail.ru>
2020-05-31 14:42:08 +0500

    ....

--- Lesson 13/final v 2.0/services/cities.py
+++ Lesson 13/final v 2.0/services/cities.py
@@ -1,101 +1,69 @@
 import sqlite3
 
-from flask import Flask, request
-from src.db import close_db
-from src.database import db
-
-app = Flask(__name__)
-app.teardown_appcontext(close_db)
+from src.exceptions import ServiceError
 
 
-class ServiceError(object):
+class CityServiceError(ServiceError):
+    service = 'cities'
+
+
+class CityDoesNotExists(CityServiceError):
     pass
 
 
-with db.connect as connection:
-    class CityServiceError(ServiceError):
-        service = 'cities'
+class CityCreationError(CityServiceError):
+    pass
 
 
-    class CityDoesNotExists(CityServiceError):
-        pass
+class CitiesService:
+    def __init__(self, connection):
+        self.connection = connection
 
-
-    class CityCreationError(CityServiceError):
-        pass
-
-
-    class CitiesService:
-        def __init__(self, connect9+8ion):
-            self.connection = connection
-
-
-# Create city
-@app.route('/cities', methods=['POST', "GET"])
-def cities():
-    if request.method == 'POST':
-        cities_json = request.json
-        name = cities_json.get('name')
-        return f'{name}', 201
-    else:
-        return '', 405
-
-
-# Получение списка всех городов из базы данных
-def read_all(self):
-    query = (
-        """
+    def read_all(self):
+        # Получение списка всех городов из базы данных
+        query = (
+            """
             SELECT *
             FROM city
             """
-    )
-    cursor = self.connection.execute(query)
-    return [dict(entry) for entry in cursor.fetchall()]
+        )
+        cursor = self.connection.execute(query)
+        return [dict(entry) for entry in cursor.fetchall()]
 
+    def create(self, name):
+        # Запись нового города в базу данных
 
-# Запись нового города в базу данных
-def create(self, name):
-    query = (
-        """
-            INSERT INTO city (name) 
-            VALUES (?)
-            ('Челябинск'),
-            ('Москва'),
-            ('Челябинск'),
-            ('Владивосток');
+        query = (
             """
-    )
+            INSERT INTO city (name) VALUES (?)
+            """
+        )
 
-    params = (name,)
+        params = (name,)
 
-    try:
-        self.connection.execute(query, params)
-        self.connection.commit()
-    except sqlite3.IntegrityError:
-        raise CityCreationError
+        try:
+            self.connection.execute(query, params)
+            self.connection.commit()
+        except sqlite3.IntegrityError:
+            raise CityCreationError
 
-    return name
+        return name
 
-
-# Получение города из базы данных
-def read(self, name):
-    query = (
-        """
+    def read(self, name):
+        # Получение города из базы данных
+        query = (
+            """
             SELECT *
             FROM city
             WHERE name = ?
             """
-    )
+        )
 
-    params = (name,)
+        params = (name,)
 
-    cursor = self.connection.execute(query, params)
-    city = cursor.fetchone()
+        cursor = self.connection.execute(query, params)
+        city = cursor.fetchone()
 
-    if city is None:
-        raise CityDoesNotExists
-    return dict(city)
-
-
-with db.connect as connection
-    cursor.execute()
+        if city is None:
+            raise CityDoesNotExists
+        return dict(city)
--- Lesson 13/final v 2.0/src/app.py
+++ Lesson 13/final v 2.0/src/app.py
@@ -1,70 +1,8 @@
-from flask import Flask, request, session
+from flask import Flask
 from src.db import close_db
 
 app = Flask(__name__)
 app.teardown_appcontext(close_db)
-
-
-# Authorization: input and output
-@app.route('/auth/login', methods=['POST'])
-def login():
-    request_json = request.json
-    email = request_json.get('email')
-    password = request_json.get('password')
-    if not email or not password:
-        return '', 400
-    else:
-        return '', 200
-
-
-# User registration
-@app.route('/users', methods=['POST', 'GET'])
-def user():
-    if request.method == 'POST':
-        user_id = session.get('user_id')
-        if user_id is None:
-            return '', 403
-        user_json = request.json
-        email = user_json.get('email')
-        password = user_json.get('password')
-        first_name = user_json.get('first_name')
-        last_name = user_json.get('last_name')
-        is_seller = user_json.get('is_seller')
-        phone = user_json.get('phone')
-        zip_code = user_json.get('zip_code')
-        city_id = user_json.get("city_id")
-        street = user_json.get('street')
-        home = user_json.get('home')
-        return '', 200
-    else:
-        if request.method == 'GET':
-            return '', 200
-
-
-@app.route('/auth/logout', methods=['POST'])
-def logout():
-    return '', 401
-
-
-#
-@app.route('/users', methods=['POST'])
-def users():
-    return '', 201
-
-
-# Create city
-@app.route('/cities', methods=['POST', "GET"])
-def cities():
-    if request.method == 'POST':
-        cities_json = request.json
-        name = cities_json.get('name')
-        return f'{name}', 201
-    else:
-        return '', 405
-
-
-
-
 
 app.debug = True
 app.run()
