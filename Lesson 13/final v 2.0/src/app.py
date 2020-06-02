765361139f5ca8af5b1c0fe3c034c3b9df37fcd5
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-06-02 21:05:37 +0500

    Update app.py

--- Lesson 13/final v 2.0/src/app.py
+++ Lesson 13/final v 2.0/src/app.py
@@ -8,8 +8,11 @@ from blueprints.image import bp as bp_image
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
+    app.register_blueprint(bp_image, url_prefix='/users')
+    app.register_blueprint(bp_image, url_prefix='/auth')
     app.register_blueprint(bp_color, url_prefix='/color')
     app.register_blueprint(bp_cities, url_prefix='/cities')
     app.register_blueprint(bp_image, url_prefix='/image')
+    app.register_blueprint(bp_image, url_prefix='/ads')
     db.init_app(app)
     return app
