68bd68d616cfa6f149ec74cac7c0db110e2ad50a
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-06-02 10:36:27 +0500

    Update users.py

--- Lesson 13/final v 2.0/blueprints/users.py
+++ Lesson 13/final v 2.0/blueprints/users.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, jsonify, request
 from flask.views import MethodView
+from werkzeug.security import generate_password_hash
+
+from database import db
+from services.users import UsersService
 
 bp = Blueprint('users', __name__)
 
@@ -10,3 +14,31 @@ class UsersView(MethodView):
 
         if not user_request_check(request_json):
             return '', 400
+        request_json["password"]= generate_password_hash(request_json["password"])
+
+        with db.connection as connection:
+            user_service = UsersService(connection)
+            try:
+                user_id = user_service.create(user_data=request_json)
+            except UserCreationError:
+                return '', 409
+
+            user = user_service.read(user_id=user_id)
+            user.update({
+                "is_seller": request_json["is_seller"],
+            })
+
+            if user["is_seller"]:
+                seller_service = SellersService(connection)
+                zipcode_service = ZipcodesService(connection)
+                try:
+                    seller_id = seller_service.create(seller_data=request_json, user_id=user_id)
+                    zipcode_service.create(zipcode_data=request_json)
+                except (SellerCreationError, ZipcodesCreationError):
+                    return '', 409
+
+                seller = seller_service.read(seller_id=seller_id)
+                user.update(seller)
+
+            return jsonify(user), 201
+
