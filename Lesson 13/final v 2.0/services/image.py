53cb809a7b26d3542415c017bc3d152c0230dd0a
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-06-02 20:09:46 +0500

    Update image.py

--- Lesson 13/final v 2.0/services/image.py
+++ Lesson 13/final v 2.0/services/image.py
@@ -7,6 +7,10 @@ class ImageServiceError(object):
     pass
 
 
+class ImageUpdateError(object):
+    pass
+
+
 class ImageService:
     def __init__(self, connection):
         self.connection = connection
@@ -24,3 +28,45 @@ class ImageService:
             self.connection.commit()
         except sqlite3.IntegrityError:
             raise ImageServiceError
+
+    def update_image(self, image_data: dict, car_id: int):
+        """Частичное редактирование данных изображения"""
+        query = (
+            """
+            UPDATE image SET title = ?, car_id = ? WHERE url = ?
+            """
+        )
+
+        params = (image_data["title"], car_id, image_data["url"])
+
+        try:
+            self.connection.execute(query, params)
+            self.connection.commit()
+        except sqlite3.IntegrityError:
+            raise ImageUpdateError
+
+    def read_image(self, car_id: int = None, ad_id: int= None) -> list:
+        """Чтение об изображениях из базы"""
+        query = (
+            """
+            SELECT image.title, image.url
+            FROM image
+            """
+        )
+        params = ()
+
+        if car_id is not None:
+            query += """WHERE car_id = ?"""
+            params = (car_id,)
+
+        elif ad_id is not None:
+            query += """
+                JOIN ad on image.car_id = ad.car_id
+            WHERE ad.id = ?
+            """
+            params = (ad_id,)
+
+        cursor = self.connection.execute(query, params)
+
+        return [dict(row) for row in cursor.fetchall()]
+
